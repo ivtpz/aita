bc9dfd0537f2f360c2b631e82065a3088f974327
Ivey Topaz <iveytopaz@gmail.com>
2017-04-11 17:44:12 +0100

    Add dummy data

---
+++ src/helpers/dummyArxivData.js
@@ -0,0 +1,39 @@
+export default {
+  feed: {
+    entry: [
+      {
+        id: '9472.4822',
+        published: '2014-05-28',
+        title: 'A Theory of Everything',
+        summary: 'Math and maths and math lorem ipsum doler for words and things and stuff. QED',
+        author: {
+          name: 'Ivey Topaz'
+        }
+      }, {
+        id: '7345.2837',
+        published: '2012-07-15',
+        title: 'So You Think You Can Math Yourself',
+        summary: 'Math and maths and math lorem ipsum doler for words and things and stuff. QED',
+        author: {
+          name: 'Adam Topaz'
+        }
+      }, {
+        id: '9485.2837',
+        published: '2014-09-28',
+        title: 'Shapes are Cool',
+        summary: 'Math and maths and math lorem ipsum doler for words and things and stuff. QED',
+        author: {
+          name: 'Tyler Kelley'
+        }
+      }, {
+        id: '8475.3425',
+        published: '2014-08-18',
+        title: 'Money Me, Please',
+        summary: 'Math and maths and math lorem ipsum doler for words and things and stuff. QED',
+        author: {
+          name: 'Shatoki Nakamoto'
+        }
+      }
+    ]
+  }
+};
