9bf335048dd4256e2cdca1c6579152d944c93810
Ivey Topaz <iveytopaz@gmail.com>
2017-04-11 17:41:05 +0100

    Add react router

--- src/main.js
+++ src/main.js
@@ -4,12 +4,19 @@ require('babel-polyfill');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import store from './store/storeConfig';
 import App from './components/App';
 
+const Root = ({ appStore }) => (
+  <Provider store={appStore}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App} />
+    </Router>
+  </Provider>
+);
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root appStore={store} />,
   document.getElementById('app')
 );
