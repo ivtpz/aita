80759c5cdbe6fb9407a948a84a25ae7bdb695505
Ivey Topaz <iveytopaz@gmail.com>
2017-04-11 20:32:11 +0100

    Use improved dummy data

--- src/helpers/dummyArxivData.js
+++ src/helpers/dummyArxivData.js
@@ -1,39 +1,282 @@
 export default {
   feed: {
-    entry: [
-      {
-        id: '9472.4822',
-        published: '2014-05-28',
-        title: 'A Theory of Everything',
-        summary: 'Math and maths and math lorem ipsum doler for words and things and stuff. QED',
-        author: {
-          name: 'Ivey Topaz'
-        }
-      }, {
-        id: '7345.2837',
-        published: '2012-07-15',
-        title: 'So You Think You Can Math Yourself',
-        summary: 'Math and maths and math lorem ipsum doler for words and things and stuff. QED',
-        author: {
-          name: 'Adam Topaz'
-        }
-      }, {
-        id: '9485.2837',
-        published: '2014-09-28',
-        title: 'Shapes are Cool',
-        summary: 'Math and maths and math lorem ipsum doler for words and things and stuff. QED',
-        author: {
-          name: 'Tyler Kelley'
-        }
-      }, {
-        id: '8475.3425',
-        published: '2014-08-18',
-        title: 'Money Me, Please',
-        summary: 'Math and maths and math lorem ipsum doler for words and things and stuff. QED',
-        author: {
-          name: 'Shatoki Nakamoto'
-        }
-      }
-    ]
+    entry: [{
+    "id": "http://arxiv.org/abs/1509.00164v1",
+    "updated": "2015-09-01T07:36:12Z",
+    "published": "2015-09-01T07:36:12Z",
+    "title": "Theorems of Tarski's Undefinability and Godel's Second Incompleteness -\n  Computationally",
+    "summary": "We show that the existence of a finitely axiomatized theory which can prove\nall the true $\\Sigma_1$ sentences may imply Godel's Second Incompleteness\nTheorem, by incorporating some bi-theoretic version of the derivability\nconditions (first discussed by Detlefsen~2001). We also argue that Tarski's\ntheorem on the undefinability of truth is Godel's first incompleteness theorem\nrelativized to definable oracles; here a unification of these two theorems is\nshown.",
+    "author": {
+      "name": "Saeed Salehi"
+    },
+    "comment": {
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+      "__prefix": "arxiv",
+      "__text": "10 pages"
+    },
+    "link":[{
+      "_href": "http://arxiv.org/abs/1509.00164v1",
+      "_rel": "alternate",
+      "_type": "text/html"
+    }, {
+      "_title": "pdf",
+      "_href": "http://arxiv.org/pdf/1509.00164v1",
+      "_rel": "related",
+      "_type": "application/pdf"
+    }],
+    "primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+      "_term": "math.LO",
+      "_scheme": "http://arxiv.org/schemas/atom",
+      "__prefix": "arxiv"
+    },
+    "category":[{
+      "_term": "math.LO",
+      "_scheme": "http://arxiv.org/schemas/atom"
+    }, {
+      "_term": "cs.LO",
+      "_scheme": "http://arxiv.org/schemas/atom"
+    }, {
+      "_term": "03F40, 03B25, 03D35, 03A05",
+      "_scheme": "http://arxiv.org/schemas/atom"
+    }]
+  }, {
+    "id": "http://arxiv.org/abs/0711.1247v2",
+    "updated": "2016-09-27T15:54:34Z",
+    "published": "2007-11-08T11:45:02Z",
+    "title": "Discovery of Quantum Hidden Variable",
+    "summary": "The first clue, in the theory of relativity, the 4-vector force acting on a\nparticle is orthogonal to the 4-vector velocity of the particle, this\northogonality means that there is some difference between the orthogonality and\nthe usual statement: the Coulomb's force (or gravitational force) acts along\nthe line joining a couple of particles (in usual 3D space), so the direction of\n4-vector Coulomb's force is carefully investigated, it is found that Maxwell's\nequations can be derived from classical Coulomb's force and the orthogonality.\nThe second clue, a 4-vector force has 4 components, because of the\northogonality of 4-vector force and 4-vector velocity, the number of\nindependent components of the 4-vector force reduces to 3, however we prove\nthat 4-vector Coulomb's force can merely provide 2 independent components, this\nsituation means that there is an undefined component accompanying the 4-vector\nCoulomb's force, hinting that this missing undefined component is a hidden\nvariable. The third clue, the best way to study the hidden variable is to\nestablish a new concept: Z-space, in which the undefined component of 4-vector\nCoulomb's force can be clearly defined as the hidden variable for the quantum\nmechanics. At the last, the undefined component is regarded as a fluctuating\nsource that contributes to Lorentz force, so that the quantum wave equation can\nbe derived out in the ensemble space of particle motion from the relativistic\nNewton's second law.",
+    "author":{
+      "name": "Huai-yang Cui"},"comment":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "This paper has been withdrawn by the author due to some wrong in its\n  chapter 7, conflicting with the current gravitational wave discovery"},"link":[{
+      "_href": "http://arxiv.org/abs/0711.1247v2",
+    "_rel": "alternate",
+    "_type": "text/html"},{
+      "_title": "pdf",
+    "_href": "http://arxiv.org/pdf/0711.1247v2",
+    "_rel": "related",
+    "_type": "application/pdf"}],"primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "_term": "physics.gen-ph",
+    "_scheme": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv"},"category":{
+      "_term": "physics.gen-ph",
+    "_scheme": "http://arxiv.org/schemas/atom"}},{
+      "id": "http://arxiv.org/abs/physics/0603091v2",
+    "updated": "2006-03-14T18:00:46Z",
+    "published": "2006-03-12T13:46:28Z",
+    "title": "A defect of the electromagnetism and it serves as a hidden variable for\n  quantum mechanics",
+    "summary": "According to the theory of relativity, the 4-vector force acting on a\nparticle is orthogonal to the 4-vector velocity of the particle, but we found\nthat the electromagnetic force of the Maxwell's theory can not completely\nsatisfy this orthogonality, this incompletion leads us to find that the\nelectromagnetic force has two independent components in the 4 dimensional space\ntime. A 4-vector force has 4 components, because of the orthogonality of\n4-vector force and 4-vector velocity, the number of independent components of\nthe force reduces to 3, while the electromagnetic force can merely provide 2\nindependent components, this situation means that there is an undefined\ncomponent accompanying the electromagnetic force. This missing undefined\ncomponent may serve as a hidden variable which quantum mechanics is eager to\nfind for a long time. The primary purpose of this paper is to strictly proof\nthat the electromagnetic force has two independent components in the 4\ndimensional space time. We also briefly discuss the possibility that the\nundefined component of the electromagnetic force serves as a hidden variable\nfor quantum mechanics.",
+    "author":{
+      "name": "Huai-yang Cui"},"comment":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "6 pages, in LaTex, 1 EPS figure"},"link":[{
+      "_href": "http://arxiv.org/abs/physics/0603091v2",
+    "_rel": "alternate",
+    "_type": "text/html"},{
+      "_title": "pdf",
+    "_href": "http://arxiv.org/pdf/physics/0603091v2",
+    "_rel": "related",
+    "_type": "application/pdf"}],"primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "_term": "physics.gen-ph",
+    "_scheme": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv"},"category":[{
+      "_term": "physics.gen-ph",
+    "_scheme": "http://arxiv.org/schemas/atom"},{
+      "_term": "physics.class-ph",
+    "_scheme": "http://arxiv.org/schemas/atom"}]},{
+      "id": "http://arxiv.org/abs/1305.5244v2",
+    "updated": "2014-10-20T19:49:41Z",
+    "published": "2013-05-22T19:47:59Z",
+    "title": "A formal framework for the study of the notion of undefined particle\n  number in quantum mechanics",
+    "summary": "It is usually stated that quantum mechanics presents problems with the\nidentity of particles, the most radical position -supported by E. Schrodinger-\nasserting that elementary particles are not individuals. But the subject goes\ndeeper, and it is even possible to obtain states with an undefined particle\nnumber. In this work we present a set theoretical framework for the description\nof undefined particle number states in quantum mechanics which provides a\nprecise logical meaning for this notion. This construction goes in the line of\nsolving a problem posed by Y. Manin, namely, to incorporate quantum mechanical\nnotions at the foundations of mathematics. We also show that our system is\ncapable of representing quantum superpositions.",
+    "author":[{
+      "name": "Newton da Costa"},{
+      "name": "Federico Holik"}],"comment":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "To appear in Synthese, 2014"},"link":[{
+      "_href": "http://arxiv.org/abs/1305.5244v2",
+    "_rel": "alternate",
+    "_type": "text/html"},{
+      "_title": "pdf",
+    "_href": "http://arxiv.org/pdf/1305.5244v2",
+    "_rel": "related",
+    "_type": "application/pdf"}],"primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "_term": "math-ph",
+    "_scheme": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv"},"category":[{
+      "_term": "math-ph",
+    "_scheme": "http://arxiv.org/schemas/atom"},{
+      "_term": "math.MP",
+    "_scheme": "http://arxiv.org/schemas/atom"},{
+      "_term": "quant-ph",
+    "_scheme": "http://arxiv.org/schemas/atom"}]},{
+      "id": "http://arxiv.org/abs/gr-qc/0006036v2",
+    "updated": "2001-03-26T17:03:23Z",
+    "published": "2000-06-12T10:23:32Z",
+    "title": "Nonthermal nature of extremal Kerr black holes",
+    "summary": "Liberati, Rothman and Sonego have recently showed that objects collapsing\ninto extremal Reissner-Nordstrom black holes do not behave as thermal objects\nat any time in their history. In particular, a temperature, and hence\nthermodynamic entropy, are undefined for them. I demonstrate that the analysis\ngoes through essentially unchanged for Kerr black holes.",
+    "author":{
+      "name": "Tony Rothman"},"doi":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "10.1016/S0375-9601(00)00515-6"},"link":[{
+      "_title": "doi",
+    "_href": "http://dx.doi.org/10.1016/S0375-9601(00)00515-6",
+    "_rel": "related"},{
+      "_href": "http://arxiv.org/abs/gr-qc/0006036v2",
+    "_rel": "alternate",
+    "_type": "text/html"},{
+      "_title": "pdf",
+    "_href": "http://arxiv.org/pdf/gr-qc/0006036v2",
+    "_rel": "related",
+    "_type": "application/pdf"}],"comment":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "Latex file, 8 pages This post corrects several misprints"},"journal_ref":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "Phys.Lett. A273 (2000) 303-309"},"primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "_term": "gr-qc",
+    "_scheme": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv"},"category":{
+      "_term": "gr-qc",
+    "_scheme": "http://arxiv.org/schemas/atom"}},{
+      "id": "http://arxiv.org/abs/physics/9909009v1",
+    "updated": "1999-09-07T07:56:24Z",
+    "published": "1999-09-07T07:56:24Z",
+    "title": "Fundamental Physical Constants and the Principle of Parametric\n  Incompleteness",
+    "summary": "The principle which allows to construct new physical theories on the basis of\nclassical mechanics by reduction of the number of its axiom without engaging\nnew postulates is formulated. The arising incompleteness of theory manifests\nitself in terms of theoretically undefinable fundamental physical constants\n$\\hbar$ and c. As an example we built up a parametric generalization of\nrelativistic theory, where the Hubble Law and the dependence of light velocity\non time are obtained.",
+    "author":{
+      "name": "S. S. Stepanov"},"comment":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "18 pages"},"link":[{
+      "_href": "http://arxiv.org/abs/physics/9909009v1",
+    "_rel": "alternate",
+    "_type": "text/html"},{
+      "_title": "pdf",
+    "_href": "http://arxiv.org/pdf/physics/9909009v1",
+    "_rel": "related",
+    "_type": "application/pdf"}],"primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "_term": "physics.gen-ph",
+    "_scheme": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv"},"category":[{
+      "_term": "physics.gen-ph",
+    "_scheme": "http://arxiv.org/schemas/atom"},{
+      "_term": "physics.ed-ph",
+    "_scheme": "http://arxiv.org/schemas/atom"}]},{
+      "id": "http://arxiv.org/abs/quant-ph/0002067v1",
+    "updated": "2000-02-24T15:25:28Z",
+    "published": "2000-02-24T15:25:28Z",
+    "title": "Rules for Integrals over Products of Distributions from Coordinate\n  Independence of Path Integrals",
+    "summary": "In perturbative calculations of quantum mechanical path integrals in\ncurvilinear coordinates, Feynman diagrams involve multiple temporal integrals\nover products of distributions, which are mathematically undefined. We derive\nsimple rules for their evaluation from the requirement of coordinate\nindependence of path integrals.",
+    "author":[{
+      "name": "H. Kleinert"},{
+      "name": "A. Chervyakov"}],"doi":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "10.1007/s100520100600"},"link":[{
+      "_title": "doi",
+    "_href": "http://dx.doi.org/10.1007/s100520100600",
+    "_rel": "related"},{
+      "_href": "http://arxiv.org/abs/quant-ph/0002067v1",
+    "_rel": "alternate",
+    "_type": "text/html"},{
+      "_title": "pdf",
+    "_href": "http://arxiv.org/pdf/quant-ph/0002067v1",
+    "_rel": "related",
+    "_type": "application/pdf"}],"comment":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "Author Information under\n  http://www.physik.fu-berlin.de/~kleinert/institution.html . Latest update of\n  paper also at\n  http://www.physik.fu-berlin.de/~kleinert/kleiner_re303/preprint.html"},"journal_ref":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "Eur. Phys. J. C 19, 743-747 (2001)"},"primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "_term": "quant-ph",
+    "_scheme": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv"},"category":{
+      "_term": "quant-ph",
+    "_scheme": "http://arxiv.org/schemas/atom"}},{
+      "id": "http://arxiv.org/abs/0902.4598v1",
+    "updated": "2009-02-26T14:26:30Z",
+    "published": "2009-02-26T14:26:30Z",
+    "title": "Comment on \"A generalized Langevin formalism of complete DNA melting\n  transition\"",
+    "summary": "We show that the calculated DNA denaturation curves for finite\n(Peyrard-Bishop-Dauxois (PBD) chains are intrinsically undefined.",
+    "author":[{
+      "name": "Titus S. van Erp"},{
+      "name": "Santiago Cuesta-Lopez"},{
+      "name": "Johannes-Geert Hagmann"},{
+      "name": "Michel Peyrard"}],"comment":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "2 pages. Accepted for EPL"},"link":[{
+      "_href": "http://arxiv.org/abs/0902.4598v1",
+    "_rel": "alternate",
+    "_type": "text/html"},{
+      "_title": "pdf",
+    "_href": "http://arxiv.org/pdf/0902.4598v1",
+    "_rel": "related",
+    "_type": "application/pdf"}],"primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "_term": "physics.bio-ph",
+    "_scheme": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv"},"category":[{
+      "_term": "physics.bio-ph",
+    "_scheme": "http://arxiv.org/schemas/atom"},{
+      "_term": "cond-mat.soft",
+    "_scheme": "http://arxiv.org/schemas/atom"}]},{
+      "id": "http://arxiv.org/abs/1502.05562v1",
+    "updated": "2015-02-19T13:23:06Z",
+    "published": "2015-02-19T13:23:06Z",
+    "title": "A New Penta-valued Logic Based Knowledge Representation",
+    "summary": "In this paper a knowledge representation model are proposed, FP5, which\ncombine the ideas from fuzzy sets and penta-valued logic. FP5 represents\nimprecise properties whose accomplished degree is undefined, contradictory or\nindeterminate for some objects. Basic operations of conjunction, disjunction\nand negation are introduced. Relations to other representation models like\nfuzzy sets, intuitionistic, paraconsistent and bipolar fuzzy sets are\ndiscussed.",
+    "author":{
+      "name": "Vasile Patrascu"},"comment":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv",
+    "__text": "The 12th International Conference Information Processing and\n  Management of Uncertainty in Knowledge-Based Systems, June 22-27, 2008,\n  Malaga, Spain"},"link":[{
+      "_href": "http://arxiv.org/abs/1502.05562v1",
+    "_rel": "alternate",
+    "_type": "text/html"},{
+      "_title": "pdf",
+    "_href": "http://arxiv.org/pdf/1502.05562v1",
+    "_rel": "related",
+    "_type": "application/pdf"}],"primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "_term": "cs.AI",
+    "_scheme": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv"},"category":{
+      "_term": "cs.AI",
+    "_scheme": "http://arxiv.org/schemas/atom"}},{
+      "id": "http://arxiv.org/abs/1511.07339v1",
+    "updated": "2015-11-23T17:50:28Z",
+    "published": "2015-11-23T17:50:28Z",
+    "title": "Fractional vortex Hilbert's Hotel",
+    "summary": "We demonstrate how the unusual mathematics of transfinite numbers, in\nparticular a nearly perfect realization of Hilbert's famous hotel paradox,\nmanifests in the propagation of light through fractional vortex plates. It is\nshown how a fractional vortex plate can be used, in principle, to create any\nnumber of \"open rooms,\" i.e. topological charges, simultaneously. Fractional\nvortex plates are therefore demonstrated to create a singularity of topological\ncharge, in which the vortex state is completely undefined and in fact\narbitrary.",
+    "author":{
+      "name": "Greg Gbur"},"link":[{
+      "_href": "http://arxiv.org/abs/1511.07339v1",
+    "_rel": "alternate",
+    "_type": "text/html"},{
+      "_title": "pdf",
+    "_href": "http://arxiv.org/pdf/1511.07339v1",
+    "_rel": "related",
+    "_type": "application/pdf"}],"primary_category":{
+      "_xmlns:arxiv": "http://arxiv.org/schemas/atom",
+    "_term": "physics.optics",
+    "_scheme": "http://arxiv.org/schemas/atom",
+    "__prefix": "arxiv"},"category":{
+      "_term": "physics.optics",
+    "_scheme": "http://arxiv.org/schemas/atom"}}]
   }
 };
