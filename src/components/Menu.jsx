437d89a1a2bc0ef07eaf0582b0688907cc8cdfa0
Ivey Topaz <iveytopaz@gmail.com>
2017-04-14 19:09:03 +0100

    Add Drawer for small menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Radium from 'radium';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import MenuLink from './MenuLink';
 import colors from '../theme/colors';
 
@@ -8,7 +11,7 @@ const styles = {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'baseline',
-    borderBottom: `2px solid lightgrey`,
+    borderBottom: '2px solid lightgrey',
     paddingBottom: 6
   },
   title: {
@@ -25,10 +28,13 @@ const styles = {
       display: 'none'
     }
   },
-  smallLinks: {
+  menuIcon: {
     fontSize: '2.4em',
     flex: '1 1 0px',
+    alignSelf: 'center',
+    paddingRight: 20,
     color: colors.Mint,
+    cursor: 'pointer',
     '@media (min-width: 925px)': {
       display: 'none'
     }
@@ -36,41 +42,81 @@ const styles = {
   rightLinkContainer: {
     display: 'flex',
     flex: '1 1 0px',
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
+    '@media (max-width: 925px)': {
+      display: 'none'
+    }
+  },
+  placeHolder: {
+    flex: '1 1 0px',
+    '@media (min-width: 925px)': {
+      display: 'none'
+    }
   }
 };
 
+// TODO: add a MUI theme
 
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
 
-const Menu = ({ leftLinks, title, rightLinks }) => (
-  <div style={styles.menu}>
-    <div
-      ref='leftlinks'
-      key={1}
-      style={styles.leftLinkContainer}>
-      {leftLinks.map((link, i) =>
-        <MenuLink {...link}
-          key={i}
-          last={i === leftLinks.length - 1}
-        />
-      )}
-    </div>
-    <div
-      ref='smalllinks'
-      key={2}
-      className='fa fa-bars'
-      style={styles.smallLinks}>
-    </div>
-    <div style={styles.title}>{title}</div>
-    <div style={styles.rightLinkContainer}>
-      {rightLinks.map((link, i) =>
-        <MenuLink {...link}
-          key={i}
-          last={i === rightLinks.length - 1}
-        />
-      )}
-    </div>
-  </div>
-);
+  handleToggle = () => this.setState({ open: !this.state.open });
+
+  handleClose = () => this.setState({ open: false });
+
+  render() {
+    const { leftLinks, title, rightLinks } = this.props;
+    return (
+      <div style={styles.menu}>
+        <div
+          ref='leftlinks'
+          key={1}
+          style={styles.leftLinkContainer}>
+          {leftLinks.map((link, i) =>
+            <MenuLink {...link}
+              key={i}
+              last={i === leftLinks.length - 1}
+            />
+          )}
+        </div>
+        <div
+          ref='menuIcon'
+          key={2}
+          className='fa fa-bars'
+          style={styles.menuIcon}
+          onClick={this.handleToggle}
+        >
+        <MuiThemeProvider>
+          <Drawer
+            docked={false}
+            width={200}
+            open={this.state.open}
+            onRequestChagne={open => this.setState({ open })}
+          >
+            {leftLinks.concat(rightLinks).map(l =>
+              <MenuItem onClick={this.handleClose}>{l.text}</MenuItem>
+            )}
+          </Drawer>
+        </MuiThemeProvider>
+        </div>
+        <div style={styles.title}>{title}</div>
+        <div style={styles.rightLinkContainer}>
+          {rightLinks.map((link, i) =>
+            <MenuLink {...link}
+              key={i}
+              last={i === rightLinks.length - 1}
+            />
+          )}
+        </div>
+        <div style={styles.placeHolder} />
+      </div>
+    );
+  }
+}
 
 export default Radium(Menu);
