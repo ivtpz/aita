0e1cfed0ad45939778925a197ab875277d3639b1
Ivey Topaz <iveytopaz@gmail.com>
2017-04-15 22:37:09 +0100

    Add router links to menu drawer

--- src/components/MenuLink.jsx
+++ src/components/MenuLink.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Radium from 'radium';
 import colors from '../theme/colors';
 
@@ -13,6 +14,7 @@ const styles = {
     padding: '4px 8px 4px 8px',
     fontSize: '1.2em',
     cursor: 'pointer',
+    textDecoration: 'none',
     ':hover': {
       color: colors.PrimaryDark
     }
@@ -24,12 +26,19 @@ const styles = {
 
 };
 
-const MenuLink = ({ action, text, last }) => (
-  <div
+const MenuLink = ({ action, text, last }) => (typeof action === 'string' ?
+  (<Link
+    key={text}
+    style={!last ? styles.link : { ...styles.link, ...styles.last }}
+    to={action}>
+    {text}
+  </Link>) :
+  (<div
+    key={text}
     style={!last ? styles.link : [styles.link, styles.last]}
     onTouchTap={action}>
     {text}
-  </div>
+  </div>)
 );
 
 export default Radium(MenuLink);
