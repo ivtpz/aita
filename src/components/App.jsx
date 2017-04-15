6f506ab6a8a1ad09061a42503c3f9aa284c0c9e2
Ivey Topaz <iveytopaz@gmail.com>
2017-04-15 22:36:10 +0100

    Use app as container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Radium, { StyleRoot } from 'radium';
 // Actions
-import { initializeLock, showAuthLock } from '../actions/auth';
-import { setSearchQuery, searchArxiv } from '../actions/arxiv';
+import { initializeLock } from '../actions/auth';
 // Components
-import SearchResultsList from './SearchResultsList';
-import SearchField from './SearchField';
 import Menu from './Menu';
-
-const styles = {
-  main: {
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  search: {
-    alignSelf: 'flex-end',
-    marginTop: 10
-  }
-};
 
 class App extends Component {
 
@@ -27,47 +13,18 @@ class App extends Component {
   }
 
   render() {
-    const {
-      openLock,
-      onSearchInput,
-      search,
-      params: {
-        filter
-      }
-    } = this.props;
     return (
       <StyleRoot>
-        <div style={styles.main}>
-          <Menu
-            leftLinks={[
-              { text: 'References', action: () => '/saved' },
-              { text: 'Recommendations', action: () => '/recommend' }
-            ]}
-            rightLinks={[{ text: 'Log in', action: openLock }]}
-            title='AITA'
-          />
-          <div style={styles.search} >
-            <SearchField
-              onSearchInput={onSearchInput}
-              search={search}
-              filter={filter || 'Search...'}
-            />
-          </div>
-          <SearchResultsList />
-        </div>
+        <Menu />
+        {this.props.children}
       </StyleRoot>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  ...state
-});
+const mapStateToProps = () => ({});
 
 const mapDispatchToProps = dispatch => ({
-  openLock: refId => dispatch(showAuthLock(refId)),
-  onSearchInput: e => dispatch(setSearchQuery(e.target.value)),
-  search: () => dispatch(searchArxiv()),
   init: () => dispatch(initializeLock())
 });
 
