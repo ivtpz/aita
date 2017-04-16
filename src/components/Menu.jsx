a3d772f5e32adc028eb50699a3ad26794fe77c57
Ivey Topaz <iveytopaz@gmail.com>
2017-04-16 15:42:02 +0100

    Add textured background, Also style transitions and drawer

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { browserHistory, Link } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import Radium from 'radium';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
 import MenuLink from './MenuLink';
-import colors from '../theme/colors';
-
-import { showAuthLock } from '../actions/auth';
+import { colors, theme } from '../theme/colors';
+// Actions
+import { showAuthLock, logout } from '../actions/auth';
 import { closeDrawer, toggleDrawer, setDrawerState } from '../actions/drawer';
 
 const styles = {
@@ -17,20 +21,67 @@ const styles = {
     justifyContent: 'space-between',
     alignItems: 'baseline',
     borderBottom: '2px solid lightgrey',
-    paddingBottom: 6
+    position: 'fixed',
+    width: '100%',
+    zIndex: 100,
+    padding: 6,
+    background: `linear-gradient(0deg, ${colors.transAccent}, ${colors.transWhite}), url('./assets/wavegrid.png') repeat left top`
   },
-  title: {
+  stickyMenu: {
+    boxShadow: '0px 1px 5px 2px darkgrey'
+  },
+  titleBase: {
     fontSize: '4em',
     fontWeight: 'bolder',
     flex: '1 1 0px',
     textAlign: 'center',
     textDecoration: 'none',
-    color: colors.PrimaryBright
+    color: colors.PrimaryBright,
+    opacity: 1,
+    transition: 'transform 0.2s ease, opacity 0.2s ease'
+  },
+  titleFaded: {
+    transform: 'scale(0.8)',
+    opacity: 0.5
+  },
+  titleHidden: {
+    opacity: 0.2,
+    transform: 'scale(0.5)'
+  },
+  iconBase: {
+    opacity: 1,
+    width: 40,
+    height: 40,
+    zIndex: 150,
+    transition: 'transform 0.3s ease, opacity 0.3s ease',
+    '@media (max-width: 1040px)': {
+      marginRight: 20
+    }
+  },
+  iconFaded: {
+    opacity: 0.5,
+    transform: 'scale(1.3)',
+    position: 'absolute',
+    top: 10,
+    left: '48%',
+    '@media (max-width: 1040px)': {
+      left: '92%'
+    }
+  },
+  iconHidden: {
+    transform: 'scale(1.1)',
+    opacity: 0.1,
+    position: 'absolute',
+    top: 15,
+    left: '48%',
+    '@media (max-width: 1040px)': {
+      left: '92%'
+    }
   },
   leftLinkContainer: {
     display: 'flex',
     flex: '1 1 0px',
-    '@media (max-width: 925px)': {
+    '@media (max-width: 1040px)': {
       display: 'none'
     }
   },
@@ -41,13 +92,21 @@ const styles = {
     paddingRight: 20,
     color: colors.Mint,
     cursor: 'pointer',
-    '@media (min-width: 925px)': {
+    '@media (min-width: 1040px)': {
       display: 'none'
+    }
+  },
+  closeButton: {
+    float: 'right',
+    zIndex: 9000,
+    backgroundColor: 'white',
+    ':hover': {
+      backgroundColor: colors.transMint
     }
   },
   placeHolder: {
     flex: '1 1 0px',
-    '@media (min-width: 925px)': {
+    '@media (min-width: 1040px)': {
       display: 'none'
     }
   }
@@ -58,6 +117,8 @@ styles.rightLinkContainer = {
   justifyContent: 'flex-end'
 };
 
+const menuMovePoint = 25;
+
 // TODO: add a MUI theme
 
 class Menu extends Component {
@@ -66,16 +127,50 @@ class Menu extends Component {
       browserHistory.push(action) :
       action());
 
+  getTitleBlock = (y, title) => {
+    const { titleBase, titleFaded, titleHidden, iconBase, iconFaded, iconHidden } = styles;
+    if (y < 10) {
+      return (<div style={titleBase}>{title}</div>);
+    } else if (y < 18) {
+      return (
+        <div>
+          <div style={[titleBase, titleFaded]}>{title}</div>
+          <div id='icon' style={[iconBase, iconHidden]}/>
+        </div>
+      );
+    } else if (y < menuMovePoint) {
+      return (
+        <div>
+          <div style={[titleBase, titleHidden]}>{title}</div>
+          <div id='icon' style={[iconBase, iconFaded]}/>
+        </div>
+      );
+    }
+    return (<div id='icon' style={iconBase}/>);
+  };
+
+
   render() {
     const leftLinks = [
-      { text: 'References', action: '/references' },
+      { text: 'My References', action: '/references' },
       { text: 'Recommendations', action: '/' }
     ];
-    const rightLinks = [{ text: 'Log in', action: this.props.openLock }];
     const title = 'AITA';
-    const { toggle, open, setDrawer } = this.props;
+    const {
+      toggle, open, setDrawer, loggedIn,
+      openLock, quit, location, scrollY
+    } = this.props;
+    const authLink = loggedIn ?
+      { text: 'Log out', action: quit } :
+      { text: 'Log in', action: openLock };
+    const rightLinks = [
+      { text: 'Search the Arxiv', action: '/' },
+      authLink
+    ];
     return (
-      <div style={styles.menu}>
+      <div
+        style={scrollY < menuMovePoint ? styles.menu : [styles.menu, styles.stickyMenu]}
+        id='menu-container'>
         <div
           ref='leftlinks'
           key={1}
@@ -84,6 +179,7 @@ class Menu extends Component {
             <MenuLink {...link}
               key={i}
               last={i === leftLinks.length - 1}
+              active={link.action === location}
             />
           )}
         </div>
@@ -94,46 +190,60 @@ class Menu extends Component {
           style={styles.menuIcon}
           onClick={toggle}
         >
-        <MuiThemeProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
           <Drawer
             docked={false}
             width={200}
             open={open}
             onRequestChagne={setDrawer}
           >
-            {leftLinks.concat(rightLinks).map(l =>
-              <MenuItem
-                onTouchTap={() => this.handleAction(l.action)}
-                key={l.text}>
-                {l.text}</MenuItem>
+            <IconButton
+              style={styles.closeButton}
+              iconStyle={{ fill: colors.Mint }}>
+              <NavigationClose />
+            </IconButton>
+            {leftLinks.concat(rightLinks).map((l, i) =>
+              <div>
+                <MenuItem
+                  onTouchTap={() => this.handleAction(l.action)}
+                  key={l.text}>
+                  {l.text}</MenuItem>
+                {i === 2 && <Divider />}
+              </div>
             )}
           </Drawer>
         </MuiThemeProvider>
         </div>
-        <Link to='/' style={styles.title}>
-          {title}
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          {this.getTitleBlock(scrollY, title)}
         </Link>
         <div style={styles.rightLinkContainer}>
           {rightLinks.map((link, i) =>
             <MenuLink {...link}
               key={i}
               last={i === rightLinks.length - 1}
+              active={link.action === location}
             />
           )}
         </div>
-        <div style={styles.placeHolder} />
+        {scrollY < menuMovePoint && <div style={styles.placeHolder} />}
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ drawer }) => ({ ...drawer });
+const mapStateToProps = ({ drawer, windowReducer, auth }) => ({
+  ...drawer,
+  ...windowReducer,
+  loggedIn: auth.loggedIn
+});
 
 const mapDispatchToProps = dispatch => ({
   openLock: refId => dispatch(showAuthLock(refId)),
   close: () => dispatch(closeDrawer()),
   toggle: () => dispatch(toggleDrawer()),
-  setDrawer: open => dispatch(setDrawerState(open))
+  setDrawer: open => dispatch(setDrawerState(open)),
+  quit: () => dispatch(logout())
 });
 
 export default connect(
