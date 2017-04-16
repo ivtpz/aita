2155a24fbf759b1543814f3ef676976154513332
Ivey Topaz <iveytopaz@gmail.com>
2017-04-16 15:42:40 +0100

    Improve responsiveness

--- src/components/MenuLink.jsx
+++ src/components/MenuLink.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
 import Radium from 'radium';
-import colors from '../theme/colors';
+import { colors } from '../theme/colors';
 
 const border = `3px solid ${colors.Mint}`;
+
+const highlighted = {
+  color: colors.PrimaryDark,
+  background: colors.transAccent
+};
 
 const styles = {
 
@@ -14,31 +19,39 @@ const styles = {
     padding: '4px 8px 4px 8px',
     fontSize: '1.2em',
     cursor: 'pointer',
-    textDecoration: 'none',
-    ':hover': {
-      color: colors.PrimaryDark
-    }
+    ':hover': highlighted
   },
-
+  active: {
+    ...highlighted,
+    cursor: 'default'
+  },
   last: {
-    borderRight: border
+    borderRight: border,
+    marginRight: 8
   }
 
 };
 
-const MenuLink = ({ action, text, last }) => (typeof action === 'string' ?
-  (<Link
-    key={text}
-    style={!last ? styles.link : { ...styles.link, ...styles.last }}
-    to={action}>
-    {text}
-  </Link>) :
-  (<div
-    key={text}
-    style={!last ? styles.link : [styles.link, styles.last]}
-    onTouchTap={action}>
-    {text}
-  </div>)
-);
+const MenuLink = ({ action, text, last, active }) => {
+  const style = [styles.link];
+  if (last) style.push(styles.last);
+  if (active) style.push(styles.active);
+  return (typeof action === 'string' ?
+    (<Link
+      key={text}
+      style={{ textDecoration: 'none' }}
+      to={action}>
+      <div style={style}>
+        {text}
+      </div>
+    </Link>) :
+    (<div
+      key={text}
+      style={style}
+      onTouchTap={action}>
+      {text}
+    </div>)
+  );
+};
 
 export default Radium(MenuLink);
