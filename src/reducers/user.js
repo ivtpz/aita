77aadab34b7d034fe2197830752641179aed5842
Ivey Topaz <iveytopaz@gmail.com>
2017-04-16 15:44:40 +0100

    Store data for user references

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,28 +1,53 @@
-const initialState = {};
+const initialState = {
+  email: null,
+  recommendations: [],
+  visibleReferences: [],
+  references: []
+};
 
 const user = (state = initialState, action) => {
   switch (action.type) {
     case 'RECEIVE_USER_DATA':
       return {
         ...state,
-        ...action.payload
+        ...action.payload,
+        visibleReferences: action.payload.referenceData || []
       };
     case 'ADD_REFERENCE': {
-      const references = state.references.concat(action.refId);
+      const references = [...state.references, action.refId];
+      const referenceData = [...state.referenceData, action.data];
       return {
         ...state,
-        references
+        references,
+        referenceData
       };
     }
     case 'REMOVE_REFERENCE': {
       const index = state.references.indexOf(action.refId);
       const references = [...state.references];
+      const referenceData = [...state.referenceData];
       references.splice(index, 1);
+      referenceData.splice(index, 1);
       return {
         ...state,
-        references
+        references,
+        referenceData
       };
     }
+    case 'FILTER_REFERENCES':
+      return {
+        ...state,
+        visibleReferences: state.referenceData.filter(r =>
+          r.title.toLowerCase().includes(action.query) ||
+          (Array.isArray(r.author) && r.author.filter(a => a.name.toLowerCase().includes(action.query)).length) ||
+          (!Array.isArray(r.author) && r.author.name.toLowerCase().includes(action.query))
+        )
+      };
+    case 'MAKE_ALL_REFS_VISIBLE':
+      return {
+        ...state,
+        visibleReferences: state.referenceData
+      };
     default:
       return state;
   }
