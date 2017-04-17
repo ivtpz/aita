41fe351c29e10d5423bb82bb649e8ecbd9c905bb
Ivey Topaz <iveytopaz@gmail.com>
2017-04-17 19:44:08 +0100

    Add material ui component state to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import user from './user';
 import arxiv from './arxiv';
-import drawer from './drawer';
+import materialUi from './materialUi';
 import windowReducer from './window';
 
 export default combineReducers({
   auth,
   user,
   arxiv,
-  drawer,
+  materialUi,
   windowReducer
 });
