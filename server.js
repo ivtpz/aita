bfc80b52f0360f5d3f8467004e8879562a58893b
Ivey Topaz <iveytopaz@gmail.com>
2017-04-23 20:03:46 +0100

    Add logging and change file structure

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import { urlencoded, json } from 'body-parser';
 // import { AuthenticationClient } from 'auth0';
 import path from 'path';
-import { findOrAddUser, addReference, removeReference } from './routes';
+import morgan from 'morgan';
+import { findOrAddUser, addReference, removeReference, sendMail, getArxivDataById } from './routes/routes';
+import { checkOrigin } from './routes/auth';
 
 const app = express();
 const port = process.env.PORT || 6543;
@@ -11,17 +13,19 @@ const port = process.env.PORT || 6543;
 //   clientId: '9ktoTjI4KL8e1qLEcTCY1SAd57LCpivK',
 //   domain: 'aita.eu.auth0.com'
 // });
-
+app.use(morgan('dev'));
 app.use(urlencoded({ extended: false }));
 app.use(json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/client/public')));
 
 app.get('/user', findOrAddUser);
 app.put('/user/add', addReference);
 app.put('/user/remove', removeReference);
+app.get('/arxiv/byid', getArxivDataById);
+app.post('/api/sendmail', checkOrigin, sendMail);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/client/public/index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
