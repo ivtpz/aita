dfdb8ca11c0b58d709ccf3c5e017e35d2774696c
Ivey Topaz <iveytopaz@gmail.com>
2017-04-30 09:15:36 +0100

    Add email template

--- helpers/email.js
+++ helpers/email.js
@@ -1,6 +1,41 @@
 const generateEmail = (name, articleData) => {
-  return true;
+  return `
+    \n<body>
+      <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
+        <tr>
+          <td align="center" valign="top">
+            <table border="0" cellpadding="20" cellspacing="0" width="600" id="emailContainer">
+              <tr>
+                <td align="center" valign="top">
+                  Hi ${name},
+                  Here are your custom recommendations from the Arxiv today:
+                </td>
+              </tr>
+              ${articleToHTML(articleData)}
+            </table>
+          </td>
+        </tr>
+      </table>
+    </body>`;
 };
+
+const articleToHTML = (articleData) =>
+  articleData.map(data =>
+    `<tr>
+      <td>
+        ${data.title}
+      </td>
+    </tr>
+    <tr>
+      <td>
+        ${data.id}
+      </td>
+    </tr>
+    <tr>
+      <td>
+        ${data.author.name}
+      </td>
+    </tr>`);
 
 export {
   generateEmail
