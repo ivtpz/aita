bf8f7807b2729b9cfd8dcf73bf8fae91eac93729
Ivey Topaz <iveytopaz@gmail.com>
2017-04-30 09:22:09 +0100

    Correct linting errors

--- helpers/email.js
+++ helpers/email.js
@@ -1,25 +1,4 @@
-const generateEmail = (name, articleData) => {
-  return `
-    \n<body>
-      <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
-        <tr>
-          <td align="center" valign="top">
-            <table border="0" cellpadding="20" cellspacing="0" width="600" id="emailContainer">
-              <tr>
-                <td align="center" valign="top">
-                  Hi ${name},
-                  Here are your custom recommendations from the Arxiv today:
-                </td>
-              </tr>
-              ${articleToHTML(articleData)}
-            </table>
-          </td>
-        </tr>
-      </table>
-    </body>`;
-};
-
-const articleToHTML = (articleData) =>
+const articleToHTML = articleData =>
   articleData.map(data =>
     `<tr>
       <td>
@@ -37,6 +16,27 @@ const articleToHTML = (articleData) =>
       </td>
     </tr>`);
 
+const generateEmail = (name, articleData) => `
+    \n<body>
+      <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
+        <tr>
+          <td align="center" valign="top">
+            <table border="0" cellpadding="20" cellspacing="0" width="600" id="emailContainer">
+              <tr>
+                <td align="center" valign="top">
+                  Hi ${name},
+                  Here are your custom recommendations from the Arxiv today:
+                </td>
+              </tr>
+              ${articleToHTML(articleData)}
+            </table>
+          </td>
+        </tr>
+      </table>
+    </body>`;
+
+
 export {
-  generateEmail
+  generateEmail,
+  articleToHTML
 };
