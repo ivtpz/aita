29237bf6297ecf0b3691b0a8f03f51804f2d8e38
Ivey Topaz <iveytopaz@gmail.com>
2017-05-01 06:42:00 +0100

    Fetch meta deta for visualization

--- client/src/containers/LandingPage.jsx
+++ client/src/containers/LandingPage.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import LandingVisual from '../components/LandingVisual';
 
-const LandingPage = () => (
-  <div>
-    <LandingVisual
-      data={[0, 1, 2]}
-      width={400}
-      height={300}
-    />
-  </div>
-);
+import { getSubjectCountData } from '../actions/arxiv';
+import { initialize, destroy } from '../actions/d3Actions';
 
-export default LandingPage;
+class LandingPage extends Component {
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    const { data, initialized, setRendered, setDestroyed } = this.props;
+    return (
+      <div>
+        <LandingVisual
+          data={data}
+          width={1000}
+          height={1000}
+          options={{
+            rendered: initialized,
+            setRendered,
+            setDestroyed
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  data: state.arxiv.metaData,
+  initialized: state.d3.initialized
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch: () => dispatch(getSubjectCountData()),
+  setRendered: () => dispatch(initialize()),
+  setDestroyed: () => dispatch(destroy())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LandingPage);
