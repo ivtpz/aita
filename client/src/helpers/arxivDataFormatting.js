bec54efbba4bdac414b34cdfe9736f485937d393
Ivey Topaz <iveytopaz@gmail.com>
2017-05-01 06:42:29 +0100

    Format meta data for d3 pack function

---
+++ client/src/helpers/arxivDataFormatting.js
@@ -0,0 +1,37 @@
+const translate = {
+  math: 'Math',
+  physics: 'Physics',
+  stat: 'Statistics',
+  'q-bio': 'Biology',
+  cs: 'Computer Science',
+  nlin: 'Non Linear',
+  'cond-mat': 'Condensed Matter',
+  noPrefix: 'Other'
+};
+
+const createHierarchy = (data) => {
+  const tree = {
+    name: 'Subjects',
+    id: 'subjects',
+    children: []
+  };
+  data.forEach(({ name, id, count }) => {
+    const general = id.split('.')[0];
+    const current = tree.children.find(c => c.id === general);
+    if (current) {
+      current.hiddenChildren.push({ name, count });
+    } else {
+      tree.children.push({
+        id: general,
+        name: translate[general],
+        hiddenChildren: [{ name, count, id }]
+      });
+    }
+    tree.children.forEach((cat) => {
+      cat.count = cat.hiddenChildren.reduce((sum, c) => sum + c.count, 0);
+    });
+  });
+  return tree;
+};
+
+export default createHierarchy;
