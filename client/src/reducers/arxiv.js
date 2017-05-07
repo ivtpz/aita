fff5b181f92d4a6d4bdffa201c4b2fb568dcb80f
Ivey Topaz <iveytopaz@gmail.com>
2017-05-07 09:01:01 +0100

    Add fetching state

--- client/src/reducers/arxiv.js
+++ client/src/reducers/arxiv.js
@@ -4,6 +4,7 @@ const initialState = {
   page: 0,
   query: undefined,
   category: { text: 'All', arxivValue: 'all' },
+  fetching: false,
   sortBy: { text: 'Relevance', arxivValue: 'relevance' },
   sortOrder: 'descending',
   metaData: [],
@@ -17,6 +18,7 @@ const arxiv = (state = initialState, action) => {
     case 'SET_ARXIV_CATEGORY':
     case 'SET_SEARCH_PAGE':
     case 'SET_ARXIV_SORTBY':
+    case 'SET_ARXIV_FETCHING':
     case 'SET_LAST_YEAR_RETREIVED':
     case 'RECEIVE_SUBJECT_METADATA':
       return {
