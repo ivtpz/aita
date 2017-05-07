ec5c23370574734cf3ace9c668da59df73a9e3b8
Ivey Topaz <iveytopaz@gmail.com>
2017-05-07 08:58:40 +0100

    Handle asrophysics category splitting

--- client/src/components/LandingVisual.js
+++ client/src/components/LandingVisual.js
@@ -1,39 +1,108 @@
 import * as d3 from 'd3';
 import d3Wrap from 'react-d3-wrap';
+import dummy from '../helpers/dummyD3Data.json';
 
-let state;
+console.log(dummy);
 
 let g;
 
 const initialValues = {};
 
-let initialData;
+let lostChildren = {};
 
-let initialized;
+let stateData;
 
-const initializeStateFromData = data => data.children.reduce((obj, c) => {
-  obj[c.id] = { hidden: true }; // eslint-disable-line no-param-reassign
-  return obj;
-}, {});
+const deepCloneAndHide = (data) => {
+  if (typeof data !== 'object') return data;
+  let clone;
+  if (!Array.isArray(data)) {
+    clone = {};
+    Object.keys(data).forEach((k) => {
+      clone[k] = deepCloneAndHide(data[k]);
+    });
+    if (clone.children) {
+      clone.hidden = true;
+    }
+  } else {
+    clone = [];
+    data.forEach(d => clone.push(deepCloneAndHide(d)));
+  }
+  return clone;
+};
 
-const updateData = (newData) => {
-  initialData.children.forEach((general) => {
-    const currGeneral = newData.children.find(c => c.id === general.id);
-    general.children.forEach((subject) => {
-      const currSubj = currGeneral.children.find(c => c.id === subject.id);
-      if (currSubj) {
-        subject.count = currSubj.count;
+const initializeData = (data) => {
+  stateData = deepCloneAndHide(data);
+};
+
+const updateData = (newData, state = stateData) => {
+  if (typeof state.count === 'number') {
+    state.count = newData.count;
+  }
+
+  if (state.children) {
+    state.children.forEach((subCat) => {
+      const newSubCat = newData.children.find(c =>
+        c.id === subCat.id
+      );
+
+      if (newSubCat) {
+        // This else if is to handle astrophysics
+        if (subCat.children && !newSubCat.children) {
+          lostChildren[subCat.id] = [...subCat.children];
+          subCat.count = newSubCat.count;
+          delete subCat.children;
+          return;
+        } else if (!subCat.children && newSubCat.children) {
+          subCat.children = lostChildren[subCat.id];
+          delete subCat.count;
+          delete lostChildren[subCat.id];
+        }
+
+        updateData(newSubCat, subCat);
       } else {
-        subject.count = 0;
+        subCat.count = 0;
       }
     });
-  });
+  }
 };
 
 const format = d3.format(',d');
 
+const findAndUpdate = (id, update) => {
+  let found = false;
+  let queue = stateData.children;
+  let queueIndex = 0;
+  while (!found && queueIndex < queue.length) {
+    const curr = queue[queueIndex];
+    if (curr.id === id) {
+      curr.hidden = update;
+      found = true;
+    } else if (curr.children) {
+      queue = queue.concat(curr.children);
+    }
+    queueIndex++;
+  }
+};
+
+// =========== TESTS FOR ELEMENT TYPE =========== //
+const isRoot = d => d.data.id === 'root';
+
+const isNotRoot = d => d.data.id !== 'root';
+
+const isMiddleNode = d => !isRoot(d) && d.children && d.depth === 1;
+
+const isSubMiddleNode = d => !isRoot(d) && !isMiddleNode(d) && d.children;
+
+const isClickableParent = d => isMiddleNode(d) || isSubMiddleNode(d);
+
+const isLeaf = d => !d.children;
+
+const isHiddenChild = d => (isLeaf(d) || isSubMiddleNode(d)) && (d.parent.data.hidden);
+
+const isDisplayedChild = d => (isLeaf(d) || isSubMiddleNode(d)) && (!d.parent.data.hidden);
+
 const showChildren = ({ data: { id } }) => {
-  state[id].hidden = false;
+  findAndUpdate(id, false);
 
   const parent = d3.selectAll('.node')
     .filter(d => d.data.id === id);
@@ -65,7 +134,7 @@ const showChildren = ({ data: { id } }) => {
 };
 
 const hideChildren = ({ data: { id } }) => {
-  state[id].hidden = true;
+  findAndUpdate(id, true);
 
   const parent = d3.selectAll('.node')
     .filter(d => d.data.id === id);
@@ -81,6 +150,12 @@ const hideChildren = ({ data: { id } }) => {
 
   const children = d3.selectAll('.node')
     .filter(d => d.parent && d.parent.data.id === id);
+
+  children.each((d) => {
+    if (d.children) {
+      hideChildren(d);
+    }
+  });
 
   children.select('text')
     .transition()
@@ -105,11 +180,18 @@ const LandingVisual = d3Wrap({
     d3.select(svg).selectAll('*').remove();
   },
   update(svg, data) {
-    console.log('UPDATE TO D3');
     if (data.children && data.children.length) {
-      if (!state) state = initializeStateFromData({ ...data });
+      if (!stateData) {
+        initializeData({ ...dummy[0] });
+        // initializeData({ ...data });
+      } else {
+        const rand = Math.floor(Math.random()*2)
+        console.log(rand)
+        updateData({ ...dummy[rand] });
+        // updateData({ ...data });
+      }
 
-      if (!initialized) {
+      if (!g) {
         const vis = d3.select(svg).attr('id', 'd3root');
         // Append background gradient definitions
         const defs = vis
@@ -120,6 +202,9 @@ const LandingVisual = d3Wrap({
 
         const middleGrad = defs.append('radialGradient')
             .attr('id', 'middleBackground');
+
+        const subMiddleGrad = defs.append('radialGradient')
+            .attr('id', 'subMiddleBackground');
 
         const leafGrad = defs.append('radialGradient')
             .attr('id', 'leafBackground');
@@ -133,8 +218,15 @@ const LandingVisual = d3Wrap({
 
         middleGrad.append('stop')
           .attr('offset', '30%')
-          .attr('stop-color', 'rgba(49, 135, 180, .9)');
+          .attr('stop-color', 'rgba(51, 167, 194, 0.95)');
         middleGrad.append('stop')
+          .attr('offset', '95%')
+          .attr('stop-color', 'rgba(77, 194, 202, 0.95)');
+
+        subMiddleGrad.append('stop')
+          .attr('offset', '30%')
+          .attr('stop-color', 'rgba(49, 135, 180, .9)');
+        subMiddleGrad.append('stop')
           .attr('offset', '95%')
           .attr('stop-color', 'rgba(51, 167, 194, 0.95)');
 
@@ -155,10 +247,8 @@ const LandingVisual = d3Wrap({
 
       // Format data for packing
       let root;
-      if (!initialized) {
-        // TODO: mark data as hidden on the data
-        initialData = { ...data };
-        root = pack(d3.hierarchy({ ...data })
+      if (!Object.keys(initialValues).length) {
+        root = pack(d3.hierarchy({ ...stateData })
           .sum(d => d.count)
           .sort((a, b) => {
             initialValues[a.data.id] = a.value;
@@ -166,29 +256,36 @@ const LandingVisual = d3Wrap({
             return b.value - a.value;
           }));
       } else {
-        updateData(data);
-        root = pack(d3.hierarchy({ ...initialData })
-          .sum(d => d.count)
-          .sort((a, b) => {
-            // Maintain original sorting order to minimize parent switching
-            const aVal = initialValues[a.data.id];
-            const bVal = initialValues[b.data.id];
-            if (!aVal || !bVal) return 0;
-            return bVal - aVal;
-          }));
+        const hi = d3.hierarchy({ ...stateData })
+        console.log('hrcy ', hi);
+        const hD = hi.sum(d => d.count);
+        console.log('pre pack ', hD)
+        root = pack(
+        hD.sort((a, b) => {
+          // Maintain original sorting order to minimize parent switching
+          const aVal = initialValues[a.data.id];
+          const bVal = initialValues[b.data.id];
+          if (!aVal || !bVal) return 0;
+          return bVal - aVal;
+        }));
+        console.log('packed ', root);
       }
+
       // ======================= Apply data ============================== //
+
       const node = g.selectAll('.node')
         .data(root.descendants());
 
+
       // ======================== DEFINE UPDATE =========================== //
+
       node
         .transition()
           .duration(2000)
           .attr('transform', d => `translate(${d.x},${d.y})`);
 
       node.select('circle')
-        .filter(d => !d.children && state[d.parent.data.id].hidden)
+        .filter(isHiddenChild)
           .style('visibility', 'hidden')
           .style('fill-opacity', 0)
           .style('stroke-opacity', 0);
@@ -199,20 +296,51 @@ const LandingVisual = d3Wrap({
           .attr('r', d => d.r);
 
       node.select('circle')
-        .filter(d => !d.children && !state[d.parent.data.id].hidden)
+        .filter(isDisplayedChild)
         .transition()
           .delay(1500)
           .style('visibility', 'visible')
           .style('fill-opacity', 1)
           .style('stroke-opacity', 1);
 
+      // For astrophysics
+      node.filter(isLeaf)
+        .select('text')
+        .style('fill', 'black')
+        .attr('style', d =>
+          (d.r > 48 ? 'font-size: 12px' : 'font-size: 10px'));
+
+      node.select('circle')
+        .filter(isLeaf)
+        .style('cursor', 'default')
+        .on('click', () => true)
+        .attr('fill', 'url(#leafBackground)');
+
+      node.filter(isSubMiddleNode)
+        .select('text')
+        .style('font-size', '18px')
+        .style('fill', 'white');
+
+      const subMidCircles = node.select('circle')
+        .filter(isSubMiddleNode);
+
+      subMidCircles
+        .style('cursor', 'pointer')
+        .attr('fill', 'url(#subMiddleBackground)');
+
+      subMidCircles.filter(d => d.data.hidden)
+        .on('click', showChildren);
+
+      subMidCircles.filter(d => !d.data.hidden)
+        .on('click', hideChildren);
+
       node.select('text')
-        .filter(d => !d.children && state[d.parent.data.id].hidden)
+        .filter(d => !d.children && d.parent.data.hidden)
           .style('opacity', 0)
           .style('visibility', 'hidden');
 
       node.select('text')
-        .filter(d => !d.children && !state[d.parent.data.id].hidden)
+        .filter(d => !d.children && !d.parent.data.hidden)
           .style('opacity', 1)
           .style('visibility', 'visible');
 
@@ -226,16 +354,22 @@ const LandingVisual = d3Wrap({
           `${d.parent && d.parent.data.id !== 'root' ? d.parent.data.name + ' - ' : ''}\
 ${d.data.name}\n${format(d.value)} Papers`);
 
-      // Remove old nodes
+// ========= EXIT ======== Remove old nodes ======================== //
+
       node.exit()
         .remove();
 
-      // =========================== DEFINE ENTER ========================= //
-      // Apply classes to new nodes
+// =========================== DEFINE ENTER ========================= //
+
+      // Apply classes
       const newNodes = node.enter()
         .append('g')
         // eslint-disable-next-line no-nested-ternary
-        .attr('class', d => (d.data.id === 'root' ? 'node' : (d.children ? 'middle node' : 'leaf node')))
+        .attr('class', (d) => {
+          if (d.data.id === 'root') return 'node';
+          if (d.children) return d.depth === 1 ? 'middle node' : 'sub-middle node';
+          return 'leaf node';
+        })
         .attr('transform', d => `translate(${d.x},${d.y})`);
 
 
@@ -247,28 +381,28 @@ ${d.data.name}\n${format(d.value)} Papers`);
 
       const circles = newNodes.append('circle');
 
+      // Apply backgrounds
       circles
         .attr('r', d => d.r)
         .attr('fill', (d) => {
           if (d.data.id === 'root') return 'url(#nodeBackground)';
-          return d.children ? 'url(#middleBackground)' : 'url(#leafBackground)';
+          if (d.children) return d.depth === 1 ? 'url(#middleBackground)' : 'url(#subMiddleBackground)';
+          return 'url(#leafBackground)';
         });
 
       // Click handlers for parents
-      circles.filter(d => d.children && d.data.id !== 'root')
+      circles.filter(isClickableParent)
         .on('click', showChildren)
         .style('cursor', 'pointer');
 
-      // Start with children hidden -- get rid of this ??
-      if (!initialized) {
-        circles.filter(d => !d.children)
-          .style('visibility', 'hidden')
-          .style('fill-opacity', 0)
-          .style('stroke-opacity', 0);
-      }
+      // Start with children hidden if parent has not been clicked
+      circles.filter(d => (isSubMiddleNode(d) || isLeaf(d)) && d.parent.data.hidden)
+        .style('visibility', 'hidden')
+        .style('fill-opacity', 0)
+        .style('stroke-opacity', 0);
 
       // Add text to all data except root
-      newNodes.filter(d => d.data.id !== 'root')
+      newNodes.filter(isNotRoot)
         .append('text')
         .attr('dy', '0.3em')
         .text(d => d.data.name.substring(0, d.r / 3));
@@ -279,26 +413,20 @@ ${d.data.name}\n${format(d.value)} Papers`);
         .style('font-size', '18px')
         .style('fill', 'white');
 
-      const leafText = newNodes.filter(d => !d.children)
-                             .select('text');
-
-      leafText.attr('style', d =>
+      newNodes.filter(isLeaf)
+        .select('text')
+        .attr('style', d =>
           (d.r > 48 ? 'font-size: 12px' : 'font-size: 10px'));
 
       // Hide child text
-      if (!initialized) {
-        leafText
-          .style('opacity', 0)
-          .style('visibility', 'hidden');
-      }
-
-      if (!initialized) initialized = true; // get rid of this ??
+      newNodes.filter(isHiddenChild)
+        .select('text')
+        .style('opacity', 0)
+        .style('visibility', 'hidden');
     }
   },
   destroy() {
     d3.select('#d3root').selectAll('*').remove();
-    state = undefined;
-    initialized = false;
   }
 });
 
