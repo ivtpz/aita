18e3e8e9010a3709d6be63ca67d564e756131415
Ivey Topaz <iveytopaz@gmail.com>
2017-05-17 07:34:45 +0100

    Style generated email

--- helpers/email.js
+++ helpers/email.js
@@ -1,18 +1,23 @@
+const formatAuthorArray = (array) => {
+  const authString = array.reduce((str, auth) => str + ', ' + auth.name, '');
+  return authString.slice(2);
+};
+
 const articleToHTML = articleData =>
-  articleData.map(data =>
+  articleData.map((data, i) =>
     `<tr>
-      <td>
-        ${data.title}
+      <td style="text-align:center;font-size:18px;border-top:${i ? '1' : '2'}px solid grey">
+        <a style="text-decoration:none" href="${data.id}" >${data.title}</a>
+      </td>
+    </tr>
+    <tr>
+      <td style="color:#4f4b4d">
+        ${Array.isArray(data.author) ? formatAuthorArray(data.author) : data.author.name}
       </td>
     </tr>
     <tr>
       <td>
-        ${data.id}
-      </td>
-    </tr>
-    <tr>
-      <td>
-        ${data.author.name}
+        ${data.summary.slice(0, 250) + '...'}
       </td>
     </tr>`);
 
@@ -21,14 +26,20 @@ const generateEmail = (name, articleData) => `
       <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
         <tr>
           <td align="center" valign="top">
-            <table border="0" cellpadding="20" cellspacing="0" width="600" id="emailContainer">
+            <table border="0" cellpadding="10" cellspacing="0" width="600" id="emailContainer">
               <tr>
-                <td align="center" valign="top">
+                <td align="center" valign="top" style="font-size:26px;background-color:papayawhip">
                   Hi ${name},
-                  Here are your custom recommendations from the Arxiv today:
+                  here are your current Arxiv recommendations from 
+                  <a href="aita.com" style="color:#F9736C;text-decoration:none">AITA</a>
                 </td>
               </tr>
-              ${articleToHTML(articleData)}
+              <tr>
+                <td align="center" valign="top" style="font-size:20px;padding-bottom:20px;background-color:papayawhip">
+                  Head over to <a href="todo" style="color:#F9736C">Your Recommendations</a> page to let us know which papers interest you, and make our system moar smartr
+                </td>
+              </tr>
+              ${articleToHTML(articleData).join('\n')}
             </table>
           </td>
         </tr>
